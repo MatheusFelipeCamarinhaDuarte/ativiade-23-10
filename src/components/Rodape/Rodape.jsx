fe66c35420317e4b51fd68641780d1798a921d6c
macirander <maciranderfilho@hotmail.com>
2023-10-23 17:11:10 -0300

    adicionando info no desenvolvido

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -2,7 +2,7 @@ export default function Rodape() {
     return (
         <footer className="bg-gray-800 text-white h-16 fixed w-full bottom-0">
             <div>
-                <p>Desenvolvido por: <span>rm552295</span></p>
+                <p>Desenvolvido por: <span>rm552295, rm551416</span></p>
                 <p>NEXT.js - React.js - Git-Hub - tailwindcss</p>
             </div>
         </footer>
