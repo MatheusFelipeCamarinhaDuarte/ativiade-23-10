9d1930e28e17bfefd3a9f2d92364204ddbafba9c
Gustavo Soares <guusoaresss@icloud.com>
2023-10-23 22:01:21 -0300

    Meu rm

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -2,7 +2,7 @@ export default function Rodape() {
     return (
         <footer className="bg-gray-800 text-white h-16 fixed w-full bottom-0">
             <div>
-                <p>Desenvolvido por: <span>rm552295, rm551416, rm551356</span></p>
+                <p>Desenvolvido por: <span>rm552295, rm551416, rm551356, rm97850</span></p>
                 <p>NEXT.js - React.js - Git-Hub - tailwindcss</p>
             </div>
         </footer>
